e8d337bbd4f6a1e7ed7d4174054dcd765a7f694a
Devendra Sathe <dsathe@usc.edu>
2023-04-21 12:01:10 -0800

    Added Tutorial Level 7 Wind + Material and changes in Cannon Tutorial

--- Assets/Scripts/DungeonMaster.cs
+++ Assets/Scripts/DungeonMaster.cs
@@ -44,8 +44,8 @@ public class DungeonMaster : MonoBehaviour
     // Level8: Introduction to enemies
     // Level9: Main Level (Previously "UIDev")
     public string[] enemyLevelNames = {"Level7","Level8","Level9","Level11","Level15"};
-    private string[] tutorialScenes = {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8"};
-    public static string[] scenes = {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8", "Level9", "Level10", "Level11","Level12","Level13","Level14", "Level15", "Level16"};
+    private string[] tutorialScenes = {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8", "Tutorial_7" };
+    public static string[] scenes = {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8", "Level9", "Level10", "Level11","Level12","Level13","Level14", "Level15", "Tutorial_7", "Level16" };
     public static List<string> levelsCompleted = new List<string>();
     public static List<string> levelsAttempted = new List<string>();
     public static List<string> completedLevelNumbers = new List<string>();
